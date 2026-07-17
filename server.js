b17e2d91a6fdce952f0c7c0c060951113947d576
rahat1517 <gmrashidulislam003@gmail.com>
2026-07-17 16:20:14 +0600

    Remove environment mode from server startup log message

--- server.js
+++ server.js
@@ -171,6 +171,5 @@ process.on('SIGTERM', () => {
         process.exit(0);
     });
 });
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
