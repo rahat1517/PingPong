d8f377169d361f1660ab69cc983527bb9758b7ff
rahat1517 <gmrashidulislam003@gmail.com>
2026-07-17 16:39:46 +0600

    Update server port configuration to default to 8080

--- server.js
+++ server.js
@@ -302,5 +302,5 @@ process.on('SIGTERM', () => {
         process.exit(0);
     });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
